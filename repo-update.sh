9b1c945e10dc956e1b38537b4817d158de73ef55
Magno Biét <magno.biet@gmail.com>
2016-05-25 11:47:52 -0300

    Only code format

--- repo-update.sh
+++ repo-update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-###########################################################
-# Script to update all git repositories in current folder #
-#                                                         #
-# v0.3                                                    #
-# By Killian Kemps                                        #
-###########################################################
+################################################################################
+# Script to update all git repositories in current folder                      #
+#                                                                              #
+# v0.3.0                                                                       #
+# By Killian Kemps                                                             #
+################################################################################
 
 list=`ls -d */`
 force_yes=false
@@ -16,51 +16,62 @@ if [ "$1" = '-f' ] || [ "$1" = '--force-yes' ]; then
 fi
 
 function update {
+
   printf "\n-- Updating $Dir"
+
   cd $Dir
   git stash > /tmp/repoUpdate
+
   editedFiles=`cat /tmp/repoUpdate`
+
   printf "\n"
   echo $editedFiles
+
   git checkout master
   git pull --rebase
   git checkout -
-  if [[ $editedFiles != *"No local changes to save"* ]]
-  then
+
+  if [[ $editedFiles != *"No local changes to save"* ]]; then
     git stash pop
   fi
+
   cd -
+
   printf "\n"
+
 }
 
-for Dir in $list
-do
-  while true
-  do
+for Dir in $list; do
+
+  while true; do
+
     # Check if the folder is a git repo
     if [[ -d "${Dir}/.git" ]]; then
 
       # Update without prompt if yes forced
       if [ "$force_yes" = true ] ; then
+
         update
         break;
+
       # Otherwise prompt user asking for repo update
       else
+
         read -p "Update $Dir? [y/n/q] " answer
 
         case $answer in
-          [yY]* ) update
-            break;;
-
-          [nN]* ) break;;
-
-          [qQ]* ) exit;;
-
-          * )  echo "Enter Y, N or Q, please.";;
+          [yY]* ) update break ;;
+          [nN]* ) break ;;
+          [qQ]* ) exit ;;
+          *     ) echo "Enter Y, N or Q, please." ;;
         esac
+
       fi
+
     else
       break
     fi
+
   done
+
 done
